1fba02362ed9298661301bf7560f4dff932c5be8
ferdoushasanpr <ferdoushasanpr@gmail.com>
2025-11-14 01:32:38 +0600

    modify(image): Place Image Storing Functionality Modified.

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,14 +1,23 @@
 import 'dart:io';
-
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:path_provider/path_provider.dart' as syspath;
+import 'package:path/path.dart' as p;
 
 import 'package:placebook/models/place.dart';
 
 class UserPlaceNotifier extends StateNotifier<List<Place>> {
   UserPlaceNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final imagePath = p.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$imagePath');
+
+    final newPlace = Place(
+      title: title,
+      image: copiedImage,
+      location: location,
+    );
     state = [newPlace, ...state];
   }
 }
